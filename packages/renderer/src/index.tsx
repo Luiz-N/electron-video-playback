8cb00407ae8e027459f90c507a9c088a69ae004d
Luiz <lneves@6river.com>
2024-06-17 10:44:22 -0500

    feat: add core webcam recording logic to app.tsx

--- packages/renderer/src/index.tsx
+++ packages/renderer/src/index.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import CssBaseline from '@mui/material/CssBaseline';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import App from './App';
 
-ReactDOM.render(
+const container = document.getElementById('app');
+const root = ReactDOM.createRoot(container!);
+
+root.render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>,
-  document.getElementById('app'),
 );
